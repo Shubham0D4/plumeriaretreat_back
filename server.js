04f54f64f01e71d99c78ae0d98b8bc35e5556951
Shubham Darekar <129769325+Shubham0D4@users.noreply.github.com>
2025-06-18 14:56:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import crypto from 'crypto';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors({
